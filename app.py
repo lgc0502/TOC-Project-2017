58e2dcbccaacd06b5ad22eac745347d6f268f35c
apple <apple113611361@gmail.com>
2018-01-03 02:47:37 +0800

    Basic

--- app.py
+++ app.py
@@ -7,40 +7,62 @@ from flask import Flask, request, send_file
 from fsm import TocMachine
 
 
-API_TOKEN = 'Your Telegram API Token'
-WEBHOOK_URL = 'Your Webhook URL'
+API_TOKEN = '488687337:AAEcNRyjTGlYzBV8Kc9ozKei2YW-SA6TibI'
+WEBHOOK_URL = 'https://f4a03846.ngrok.io/hook'
 
 app = Flask(__name__)
 bot = telegram.Bot(token=API_TOKEN)
 machine = TocMachine(
     states=[
-        'user',
-        'state1',
-        'state2'
+        'which',
+        'horoscope',
+        'divination',
+        '2018',
+        '2018query'
     ],
     transitions=[
         {
             'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state1',
-            'conditions': 'is_going_to_state1'
+            'source': 'which',
+            'dest': 'which',
+            'conditions': 'which_to_which'
         },
         {
             'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state2',
-            'conditions': 'is_going_to_state2'
+            'source': 'which',
+            'dest': 'horoscope',
+            'conditions': 'is_going_to_horoscope'
         },
+        {
+            'trigger': 'advance',
+            'source': 'which',
+            'dest': 'divination',
+            'conditions': 'is_going_to_divination'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'which',
+            'dest': '2018',
+            'conditions': 'is_going_to_2018'
+        },
+        {
+            'trigger': 'advance',
+            'source': '2018',
+            'dest': '2018query',
+            'conditions': 'is_going_to_2018query'
+        },
+
         {
             'trigger': 'go_back',
             'source': [
-                'state1',
-                'state2'
+                'horoscope',
+                'divination',
+                '2018query'
             ],
-            'dest': 'user'
+            'dest': 'which'
         }
     ],
-    initial='user',
+    initial='which',
     auto_transitions=False,
     show_conditions=True,
 )
--- fsm.py
+++ fsm.py
@@ -1,5 +1,7 @@
 from transitions.extensions import GraphMachine
 
+import sys
+from io import BytesIO
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
@@ -7,25 +9,80 @@ class TocMachine(GraphMachine):
             model = self,
             **machine_configs
         )
-
-    def is_going_to_state1(self, update):
+    def which_to_which(self, update):
         text = update.message.text
-        return text.lower() == 'go to state1'
+        return (text!='1' and text !='2' and text != '3')
 
-    def is_going_to_state2(self, update):
+    def is_going_to_horoscope(self, update):
         text = update.message.text
-        return text.lower() == 'go to state2'
+        return text == '1'
 
-    def on_enter_state1(self, update):
-        update.message.reply_text("I'm entering state1")
-        self.go_back(update)
+    def is_going_to_divination(self, update):
+        text = update.message.text
+        return text == '2'
 
-    def on_exit_state1(self, update):
+    def is_going_to_2018(self, update):
+        text = update.message.text
+        return text == '3'
+    def is_going_to_2018query(self, update):
+        text = update.message.text
+        return (text == '1' or text == '2' or text == '3' or text == '4' or text == '5' or text == '6' or text == '7' or text == '8' or text == '9' or text == '10' or text == '11' or text == '12')
+    
+    def on_enter_which(self, update):
+        update.message.reply_text(
+                "歡迎來到占卜星球\n"
+                "查看今日星座運勢請按1\n"
+                "查看占卜請按2\n"
+                "查看2018年運勢請按3"
+                )
+
+    def on_enter_horoscope(self, update):
+        update.message.reply_text(
+                "請輸入星座編號\n"
+                "1. 牡羊座( 3/21 - 4/20 )\n"
+                "2. 金牛座( 4/21 - 5/21 )\n"
+                "3. 雙子座( 5/22 - 6/21 )\n"
+                "4. 巨蟹座( 6/22 - 7/22 )\n"
+                "5. 獅子座( 7/23 - 8/23 )\n"
+                "6. 處女座( 8/24 - 9/23 )\n"
+                "7. 天秤座( 9/24 - 10/23 )\n"
+                "8. 天蝎座( 10/24 - 11/22 )\n"
+                "9. 射手座( 11/23 - 12/21 )\n"
+                "10.魔羯座( 12/22 - 1/20 )\n"
+                "11.水瓶座( 1/21 - 2/19 )\n"
+                "12.雙鱼座( 2/20 - 3/20 )")
+        #self.go_back(update)
+
+    def on_exit_horoscope(self, update):
         print('Leaving state1')
 
-    def on_enter_state2(self, update):
+    def on_enter_divination(self, update):
         update.message.reply_text("I'm entering state2")
-        self.go_back(update)
+        #self.go_back(update)
 
-    def on_exit_state2(self, update):
+    def on_exit_divination(self, update):
         print('Leaving state2')
+    
+    def on_enter_2018(self, update):
+        update.message.reply_text(
+                "請輸入星座編號\n"
+                "1. 牡羊座( 3/21 - 4/20 )\n"
+                "2. 金牛座( 4/21 - 5/21 )\n"
+                "3. 雙子座( 5/22 - 6/21 )\n"
+                "4. 巨蟹座( 6/22 - 7/22 )\n"
+                "5. 獅子座( 7/23 - 8/23 )\n"
+                "6. 處女座( 8/24 - 9/23 )\n"
+                "7. 天秤座( 9/24 - 10/23 )\n"
+                "8. 天蝎座( 10/24 - 11/22 )\n"
+                "9. 射手座( 11/23 - 12/21 )\n"
+                "10.魔羯座( 12/22 - 1/20 )\n"
+                "11.水瓶座( 1/21 - 2/19 )\n"
+                "12.雙鱼座( 2/20 - 3/20 )")
+    def on_enter_2018query(self, update):
+        text = update.message.text
+        if text == '1':
+            update.message.reply_text("  2018 年裡歲星來到了羊座疾厄宮，可以預料這一年裡運勢變化精采無比，當中不乏好運與壞運的逆轉，有機會柳暗花明，在最需要的時候得到好運。\n  獨立經營事業當頭家的，考驗中能讓生意蒸蒸日上，能開創新的契機；固定受薪族的變化較不明顯，但作為核心幕僚的人，今年表現不俗，能蒙受多位貴人提攜。得特別注意八月火星逆入魔羯宮帶來的情緒問題。感情變化上半年多冷靜下半年多險阻，考驗著彼此的信念，如果過於患得患失則很可能有一波三折的走向。") 
+        elif text == '2':  
+            update.message.reply_text("金牛們在新的一年裡很可能從本來就事論事、實事求是的理性腦，一下子轉身成了風花雪月的浪漫腦，任何事都可能觸動你的愁緒，遠在異鄉打拼的人會有遇見故知的機緣，單身者特別容易感到孤單，上半年波折較多的反倒能在下半年苦盡甘來，2018 整體來說感情上夠你折騰，但相對也能嘗到愈發甜美的果實。")
+            update.message.reply_text("工作上表現平盤，雖然暗藏凶險，但大都能化險為夷，比起做事，職場中的牛座更要學著靈活彈性的做人")
+        self.go_back(update)
